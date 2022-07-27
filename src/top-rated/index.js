3c1bffa03588a842e8ff7573b70b22130350c6de
kaechesbord <damirlihtneri6@gmail.com>
2022-07-27 21:57:59 +0200

    nema vise shorter

--- src/top-rated/index.js
+++ src/top-rated/index.js
@@ -8,7 +8,7 @@ export const ŠvercKomerc = () => {
     console.log(true) //Nisam znao sta ovdje da stavim ali koristim funkciju da prebacim element."neki atribut iz data results" u MovieDetails.js
 }
 
-const TopMovies = () => {
+const TopMovies = (props) => {
     const [movies, setMovies] = useState([])
     const [search, setSearch] = useState("")
     const [loading, setLoading] = useState(true)
@@ -18,16 +18,15 @@ const TopMovies = () => {
             const data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=1")
             setLoading(false)
             setMovies(data.data.results)
-            const shorter = data.data.results;
-            shorter.map(element => <ŠvercKomerc title = {element.original_title}/>)
         }
         fetchData()
     }, [])
     
   return (
     <div>
+      {movies.map(element => <ŠvercKomerc title = {element.original_title}/>)}
       <Search search = {search} setSearch = {setSearch}/>
-      <Movies movies = {movies} loading = {loading} />
+      <Movies movies = {movies} loading = {loading}/>
     </div>
   )
 }
