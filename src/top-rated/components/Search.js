735e7881f210df63aeaa39ae5b5103b3cdff872f
kaechesbord <damirlihtneri6@gmail.com>
2022-07-31 11:45:05 +0200

    Search radi Modal ne

--- src/top-rated/components/Search.js
+++ src/top-rated/components/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-const Search = (props) => {
-    
+const Search = ({ search, setSearch }) => {
+  
   return (
     <nav className="navbar navbar-dark bg-dark justify-content-between position-fixed fixed-top">
       <a className="navbar-brand" href="www.google.com">IGTV</a>
@@ -9,19 +9,11 @@ const Search = (props) => {
         <input
           className="form-control mr-sm-2"
           type="search"
-          value={props.search}
-          onChange={(event) => props.setSearch(event.target.value)}
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
           placeholder="Search"
           aria-label="Search"
         ></input>
-        {props.movies.filter((movie) => {  
-          if(props.search === ""){
-            return movie
-          }
-          else if(movie.props.title.toLowerCase().includes(props.search.toLowerCase())){
-            return movie
-          }
-        })}
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
           Search
         </button>
--- src/top-rated/index.js
+++ src/top-rated/index.js
@@ -3,28 +3,39 @@ import axios from 'axios';
 import Search from './components/Search';
 import Movies from './components/Movies';
 import './components/comp.css'
+import MovieDetails from './components/MovieDetails';
 
-export const ŠvercKomerc = () => {
-    console.log(true) //Nisam znao sta ovdje da stavim ali koristim funkciju da prebacim element."neki atribut iz data results" u MovieDetails.js
-}
 
-const TopMovies = (props) => {
+const TopMovies = () => {
     const [movies, setMovies] = useState([])
     const [search, setSearch] = useState("")
     const [loading, setLoading] = useState(true)
+    const [originalMovies, setOriginalMovies] = useState([])
 
     useEffect(() => {
-        const fetchData = async() => {
-            const data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=1")
-            setLoading(false)
-            setMovies(data.data.results)
-        }
-        fetchData()
-    }, [])
-    
+      const fetchData = async() => {
+          const data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=1")
+          setLoading(false)
+          setMovies(data.data.results)
+          setOriginalMovies(data.data.results)
+      }
+      fetchData()
+  }, [])
+  
+useEffect(()=>{
+  if(search.length > 0) {
+    const slicedMovies = originalMovies.slice();
+    const moviesForSearch = [...slicedMovies]
+    const filteredMovies = moviesForSearch.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()))
+    setMovies(filteredMovies)
+  }
+  else {
+      setMovies(originalMovies)
+  }
+}, [search])
   return (
     <div>
-      {movies.map(element => <ŠvercKomerc titles = {element.original_title}/>)}
+      {movies.map(element => <MovieDetails title = {element.original_title}/>)}
       <Search search = {search} setSearch = {setSearch}/>
       <Movies movies = {movies} loading = {loading}/>
     </div>
